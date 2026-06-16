eaba77343416e04ae0d99bed8aff86238aefe53c
ludicious-claw <ludicious_claw@tutamail.com>
2026-06-16 19:25:11 +0500

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -1,4 +1,4 @@
-local httpService = game:GetService('HttpService')
+local httpService = cloneref(game:GetService('HttpService'))
 local ThemeManager = {} do
 	ThemeManager.Folder = 'LinoriaLibSettings'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
